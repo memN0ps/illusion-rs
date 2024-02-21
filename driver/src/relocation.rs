8bdbdef9a014f82065af731ce6be89aa922e477b
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-02-21 19:04:30 +1300

    Added a file to zap relocation, no major changes. Neaten format

---
+++ driver/src/relocation.rs
@@ -0,0 +1,46 @@
+use {
+    log::debug,
+    uefi::{
+        prelude::{Boot, SystemTable},
+        proto::loaded_image::LoadedImage,
+    },
+};
+
+/// Nullifies the relocation table of the loaded UEFI image to prevent relocation.
+///
+/// This function manipulates the loaded image's PE header to zero out the relocation table,
+/// preventing UEFI from applying patches to the hypervisor code during the transition
+/// from physical-mode to virtual-mode addressing by the operating system.
+///
+/// # Arguments
+///
+/// * `system_table` - Reference to the UEFI System Table.
+///
+/// # Returns
+///
+/// The result of the operation. Returns `uefi::Result::SUCCESS` on success, or an error
+pub fn zap_relocations(system_table: &SystemTable<Boot>) -> uefi::Result<()> {
+    let boot_service = system_table.boot_services();
+
+    // Obtain the current loaded image protocol.
+    let loaded_image =
+        boot_service.open_protocol_exclusive::<LoadedImage>(boot_service.image_handle())?;
+
+    // Extract the image base address and size.
+    let (image_base, image_size) = loaded_image.info();
+    let image_base = image_base as usize;
+    let image_range = image_base..image_base + image_size as usize;
+
+    // Log the image base address range for debugging purposes.
+    debug!("Image base: {:#x?}", image_range);
+
+    // Unsafe block to directly modify the PE header of the loaded image.
+    // This operation nullifies the relocation table to prevent UEFI from
+    // applying relocations to the hypervisor code.
+    unsafe {
+        *((image_base + 0x128) as *mut u32) = 0; // Zero out the relocation table offset.
+        *((image_base + 0x12c) as *mut u32) = 0; // Zero out the relocation table size.
+    }
+
+    Ok(())
+}
