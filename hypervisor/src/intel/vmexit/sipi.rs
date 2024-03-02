fe611c4af9cff759081954935d8dc986b2965e4d
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-03-02 19:46:19 +1300

    SIPI should also update guest registers

--- hypervisor/src/intel/vmexit/sipi.rs
+++ hypervisor/src/intel/vmexit/sipi.rs
@@ -22,41 +22,19 @@ use {
 ///
 /// # Arguments
 ///
-/// - `_guest_registers`: A mutable reference to the guest's general-purpose registers. Currently unused.
+/// - `guest_registers`: A mutable reference to the guest's general-purpose registers. Currently unused.
 ///
 /// # Returns
 ///
 /// Returns `ExitType::Continue` to indicate the VM should continue execution.
-pub fn handle_sipi_signal(_guest_registers: &mut GuestRegisters) -> ExitType {
-    //
-    // Then, emulate effects of SIPI by making further changes.
-    //
-    // "For a start-up IPI (SIPI), the exit qualification contains the SIPI
-    //  vector information in bits 7:0. Bits 63:8 of the exit qualification are
-    //  cleared to 0."
-    // See: 27.2.1 Basic VM-Exit Information
-    //
+pub fn handle_sipi_signal(guest_registers: &mut GuestRegisters) -> ExitType {
     let vector = vmread(vmcs::ro::EXIT_QUALIFICATION);
 
-    //
-    // "At the end of the boot-strap procedure, the BSP sets ... broadcasts a
-    //  SIPI message to all the APs in the system. Here, the SIPI message contains
-    //  a vector to the BIOS AP initialization code (at 000VV000H, where VV is the
-    //  vector contained in the SIPI message)."
-    //
-    // See: 8.4.3 MP Initialization Protocol Algorithm for MP Systems
-    //
     vmwrite(vmcs::guest::CS_SELECTOR, vector << 8);
     vmwrite(vmcs::guest::CS_BASE, vector << 12);
-    vmwrite(vmcs::guest::RIP, 0u64);
+    guest_registers.rip = 0x0u64;
+    vmwrite(vmcs::guest::RIP, guest_registers.rip);
 
-    //
-    // Done. Note that the 2nd SIPI will be ignored if that occurs after this.
-    //
-    // "If a logical processor is not in the wait-for-SIPI activity state when a
-    //  SIPI arrives, no VM exit occurs and the SIPI is discarded"
-    // See: 25.2 OTHER CAUSES OF VM EXITS
-    //
     let vmx_active = 0x0u64;
     vmwrite(vmcs::guest::ACTIVITY_STATE, vmx_active);
 
