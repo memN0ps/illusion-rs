a9917c12cb988d870f68eeb332795a9ed1816ae7
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-03-27 03:02:20 +1300

    Syscall hook test added. (Tests have not been run yet)

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -11,16 +11,24 @@ extern crate alloc;
 
 pub mod windows;
 
-use crate::windows::nt::types::{
-    FILE_ACCESS_RIGHTS, FILE_FLAGS_AND_ATTRIBUTES, FILE_SHARE_MODE, HANDLE, IO_STATUS_BLOCK,
-    NTCREATEFILE_CREATE_DISPOSITION, NTCREATEFILE_CREATE_OPTIONS, NTSTATUS, OBJECT_ATTRIBUTES,
-};
 use {
-    crate::windows::hooks::ept::{
-        hook::{Hook, HookType},
-        inline::InlineHookType,
+    crate::windows::{
+        hooks::{
+            ept::{
+                hook::{Hook, HookType},
+                inline::InlineHookType,
+                manager::HookManager,
+            },
+            ssdt::ssdt_hook::SsdtHook,
+        },
+        nt::types::{
+            FILE_ACCESS_RIGHTS, FILE_FLAGS_AND_ATTRIBUTES, FILE_SHARE_MODE, HANDLE,
+            IO_STATUS_BLOCK, NTCREATEFILE_CREATE_DISPOSITION, NTCREATEFILE_CREATE_OPTIONS,
+            NTSTATUS, OBJECT_ATTRIBUTES,
+        },
     },
     alloc::boxed::Box,
+    alloc::vec,
     core::{
         ffi::c_void,
         mem, ptr,
@@ -29,9 +37,7 @@ use {
     hypervisor::{error::HypervisorError, intel::ept::Ept},
 };
 
-/// The entry point for the hypervisor.
-///
-/// This function is the main entry point for the hypervisor and is called by the bootloader.
+/// A test function to demonstrate how to install an inline EPT hook on a Windows kernel function.
 ///
 /// # Arguments
 ///
@@ -39,6 +45,9 @@ use {
 /// * hook_handler - The handler function to be called when the hooked function is executed.
 /// * hook_type - The type of hook to be installed.
 ///
+/// # Returns
+///
+/// Returns `Ok(())` if the hook was successfully installed, `Err(HypervisorError)` otherwise.
 fn test_windows_kernel_inline_ept_hook(
     original_va: u64,
     hook_handler: *const (),
@@ -53,7 +62,53 @@ fn test_windows_kernel_inline_ept_hook(
         MM_IS_ADDRESS_VALID_ORIGINAL.store(inline_hook.trampoline_address(), Ordering::Relaxed);
     }
 
+    // Create a new hook manager and add the `mm_is_address_valid` hook to it.
+    // Must be stored in a global variable to prevent it from being dropped. maybe in `SharedData` from vmexit handler or after returning from there.
+    let hook_manager = HookManager::new(vec![mm_is_address_valid]);
+
     Ok(())
+}
+
+/// A test function to demonstrate how to install an syscall inline EPT hook on a Windows kernel function.
+///
+/// # Arguments
+///
+/// * `api_number` - The API number of the function to hook.
+/// * `get_from_win32k` - Whether to get the function from the Win32k table instead of the NT table.
+/// * `kernel_base` - The base address of the kernel in memory.
+/// * `kernel_size` - The size of the kernel memory space.
+///
+/// # Returns
+///
+/// Returns `Ok(())` if the hook was successfully installed, `Err(HypervisorError)` otherwise.
+fn test_windows_kernel_syscall_inline_ept_hook(
+    api_number: i32,
+    get_from_win32k: bool,
+    kernel_base: *const u8,
+    kernel_size: usize,
+) -> Result<(), HypervisorError> {
+    // Example 2: Syscall EPT Hook NtCreateFile via SSDT Function Entry
+
+    let ssdt_nt_create_file_addy = SsdtHook::find_ssdt_function_address(
+        api_number,
+        get_from_win32k,
+        kernel_base,
+        kernel_size,
+    )?;
+
+    let nt_create_file_syscall_hook = Hook::hook_function_ptr(
+        ssdt_nt_create_file_addy.function_address as _,
+        test_nt_create_file as *const (),
+    )
+    .ok_or(HypervisorError::HookError)?;
+
+    if let HookType::Function { ref inline_hook } = nt_create_file_syscall_hook.hook_type {
+        NT_CREATE_FILE_ORIGINAL.store(inline_hook.trampoline_address(), Ordering::Relaxed);
+    }
+
+    // Create a new hook manager and add the `nt_create_file_syscall_hook` hook to it.
+    // Must be stored in a global variable to prevent it from being dropped. maybe in `SharedData` from vmexit handler or after returning from there.
+    let hook_manager = HookManager::new(vec![nt_create_file_syscall_hook]);
 }
 
 /// A global atomic pointer to hold the original `mm_is_address_valid` function.
--- tests/src/windows/hooks/ssdt/mod.rs
+++ tests/src/windows/hooks/ssdt/mod.rs
@@ -1,2 +1,2 @@
-mod ssdt_find;
-mod ssdt_hook;
+pub mod ssdt_find;
+pub mod ssdt_hook;
