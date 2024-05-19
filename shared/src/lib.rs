6866046e6f6413c250af91e5efc78633becb4208
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-05-19 19:04:20 +1200

    Update lib.rs

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -18,6 +18,26 @@ pub enum Commands {
     Invalid,
 }
 
+impl Commands {
+    /// Converts a `u64` value to a `Commands` enum variant.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - The `u64` value to convert.
+    ///
+    /// # Returns
+    ///
+    /// * `Commands` - The corresponding `Commands` enum variant.
+    pub fn from_u64(value: u64) -> Commands {
+        match value {
+            0 => Commands::EnableKernelInlineHook,
+            1 => Commands::EnableSyscallInlineHook,
+            2 => Commands::DisablePageHook,
+            _ => Commands::Invalid,
+        }
+    }
+}
+
 /// Structure representing the data sent by the client to the hypervisor.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ClientData {
@@ -25,4 +45,28 @@ pub struct ClientData {
     pub syscall_number: i32,
     pub get_from_win32k: bool,
     pub function_hash: u32,
+}
+
+impl ClientData {
+    /// Converts `ClientData` to a pointer.
+    ///
+    /// # Returns
+    ///
+    /// * `u64` - The pointer to the `ClientData`.
+    pub fn as_ptr(&self) -> u64 {
+        self as *const ClientData as u64
+    }
+
+    /// Converts a pointer to `ClientData`.
+    ///
+    /// # Arguments
+    ///
+    /// * `ptr` - The pointer to the `ClientData`.
+    ///
+    /// # Returns
+    ///
+    /// * `&'static ClientData` - The reference to the `ClientData`.
+    pub fn from_ptr(ptr: u64) -> &'static ClientData {
+        unsafe { &*(ptr as *const ClientData) }
+    }
 }
