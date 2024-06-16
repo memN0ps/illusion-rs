33f84e75609eb3ce8d009ee7778bf334f0f45ed8
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-06-16 13:32:13 +1200

    Update global_const.rs

--- hypervisor/src/global_const.rs
+++ hypervisor/src/global_const.rs
@@ -1,8 +1,13 @@
+use uefi::table::boot::MemoryType;
+
 /// The size of the heap in bytes.
-pub const HEAP_SIZE: usize = 0x5000000; // 80 MB
+pub const HEAP_SIZE: usize = 0x180000;
 
 /// The size of the stack in bytes.
-pub const STACK_SIZE: usize = 0x3000; // 48 MB
+pub const STACK_NUMBER_OF_PAGES: usize = 0x20;
+
+/// The memory type for the stack allocated pages
+pub const STACK_MEMORY_TYPE: MemoryType = MemoryType::RUNTIME_SERVICES_DATA;
 
 /// The maximum number of hooks supported by the hypervisor. Change this value as needed.
 pub const MAX_HOOK_ENTRIES: usize = 64;
