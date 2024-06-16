ff707946b57ee9bdc72d783b1517448e4f13997e
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-06-16 13:51:46 +1200

    Update virtualize.rs

--- uefi/src/virtualize.rs
+++ uefi/src/virtualize.rs
@@ -4,14 +4,8 @@
 //! Credits to Satoshi Tanda: https://github.com/tandasat/Hello-VT-rp/blob/main/hypervisor/src/switch_stack.rs
 
 use {
-    alloc::alloc::handle_alloc_error,
-    core::{alloc::Layout, arch::global_asm},
-    hypervisor::{
-        allocator::allocate_host_stack,
-        global_const::STACK_NUMBER_OF_PAGES,
-        intel::{capture::GuestRegisters, page::Page},
-        vmm::start_hypervisor,
-    },
+    core::arch::global_asm,
+    hypervisor::{allocator::allocate_host_stack, global_const::STACK_NUMBER_OF_PAGES, intel::capture::GuestRegisters, vmm::start_hypervisor},
     log::debug,
 };
 
