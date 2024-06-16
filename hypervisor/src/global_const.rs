3e853e4eec62d27ba92d049cd60fee3201e3c993
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-06-16 18:44:32 +1200

    Update global_const.rs

--- hypervisor/src/global_const.rs
+++ hypervisor/src/global_const.rs
@@ -4,7 +4,7 @@ use uefi::table::boot::MemoryType;
 pub const HEAP_SIZE: usize = 0x180000;
 
 /// The size of the stack in bytes.
-pub const STACK_NUMBER_OF_PAGES: usize = 0x4000;
+pub const STACK_NUMBER_OF_PAGES: usize = 0x1000;
 
 /// The memory type for the stack allocated pages
 pub const STACK_MEMORY_TYPE: MemoryType = MemoryType::RUNTIME_SERVICES_DATA;
