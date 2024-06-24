99551cab388a4f77653ce22db238b561284973ad
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-06-24 18:50:19 +1200

    Update global_const.rs

--- hypervisor/src/global_const.rs
+++ hypervisor/src/global_const.rs
@@ -2,18 +2,21 @@
 ///
 /// This value is set to 1 for testing purposes but can be adjusted up to 32 or more based on the system.
 /// Adjusting this value will increase the total heap size accordingly.
-const DEFAULT_LOGICAL_PROCESSORS: usize = 1;
+const DEFAULT_LOGICAL_PROCESSORS: usize = 32;
 
 /// The total size of the heap in bytes, shared among all processors.
 ///
 /// This base heap size is for 1 processor, calculated as:
-/// 64 * 1024 * 1024 = 67,108,864 bytes (64 MB)
+/// 16 * 1024 * 1024 = 16,777,216 bytes (16 MB)
+///
+/// For 16 processors, the heap size would be:
+/// 16 * 1024 * 1024 * 16 = 268,435,456 bytes (256 MB)
 ///
 /// For 32 processors, the heap size would be:
-/// 64 * 1024 * 1024 * 32 = 2,147,483,648 bytes (2 GB)
+/// 16 * 1024 * 1024 * 32 = 536,870,912 bytes (512 MB)
 ///
 /// By adjusting the number of logical processors, the heap size will scale accordingly.
-pub const TOTAL_HEAP_SIZE: usize = 64 * 1024 * 1024 * DEFAULT_LOGICAL_PROCESSORS;
+pub const TOTAL_HEAP_SIZE: usize = 16 * 1024 * 1024 * DEFAULT_LOGICAL_PROCESSORS;
 
 /// The number of pages for the stack per processor/core.
 ///
